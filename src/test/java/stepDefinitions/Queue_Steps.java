41a31d1f5bbb3027468622120f1c3762661a88ef
AyushaGami <patoriyaa@yahoo.in>
2025-05-28 16:30:45 -0400

    added Feature file and Step defination

--- src/test/java/stepDefinitions/Queue_Steps.java
+++ src/test/java/stepDefinitions/Queue_Steps.java
@@ -1,5 +1,138 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Queue_Steps {
 
+	@Given("Queue-User is in the Home page after logging in using credentials from Excel {int}")
+	public void queue_user_is_in_the_home_page_after_logging_in_using_credentials_from_excel(Integer int1) {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks on Get Started button under the section Queue")
+	public void user_clicks_on_get_started_button_under_the_section_queue() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User lands on the Queue page")
+	public void user_lands_on_the_queue_page() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("User is in the Queue page after logging in using credentials from Excel {int}")
+	public void user_is_in_the_queue_page_after_logging_in_using_credentials_from_excel(Integer int1) {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks on the hyperlink Implementation of Queue in Python")
+	public void user_clicks_on_the_hyperlink_implementation_of_queue_in_python() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User lands on the Implementation of Queue in Python page")
+	public void user_lands_on_the_implementation_of_queue_in_python_page() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("User is in the Implementation of Queue in Python page")
+	public void user_is_in_the_implementation_of_queue_in_python_page() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Queue - User clicks on the Practice Questions from the side navigation bar")
+	public void queue_user_clicks_on_the_practice_questions_from_the_side_navigation_bar() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Queue - User lands on the Practice Questions page and is able to view the content successfully")
+	public void queue_user_lands_on_the_practice_questions_page_and_is_able_to_view_the_content_successfully() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Queue - User clicks on the Try here>>> button")
+	public void queue_user_clicks_on_the_try_here_button() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Queue - User lands on the Try Editor page.")
+	public void queue_user_lands_on_the_try_editor_page() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("Queue - User is in Try Editor page from Implementation of Queue in Python page")
+	public void queue_user_is_in_try_editor_page_from_implementation_of_queue_in_python_page() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Queue - User clicks on Run Button without entering the code")
+	public void queue_user_clicks_on_run_button_without_entering_the_code() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Queue - An Alert window with the error message is displayed.")
+	public void queue_an_alert_window_with_the_error_message_is_displayed() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks on Run Button after entering a invalid python code from Excel {int} for Queue Module")
+	public void user_clicks_on_run_button_after_entering_a_invalid_python_code_from_excel_for_queue_module(Integer int1) {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("An Alert window with the error message from Excel {int} for Queue Module is displayed")
+	public void an_alert_window_with_the_error_message_from_excel_for_queue_module_is_displayed(Integer int1) {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks on Run Button after entering a valid python code from Excel {int} for Queue Module")
+	public void user_clicks_on_run_button_after_entering_a_valid_python_code_from_excel_for_queue_module(Integer int1) {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Correct output is displayed in the console from Excel {int} for Queue Module")
+	public void correct_output_is_displayed_in_the_console_from_excel_for_queue_module(Integer int1) {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks on the hyperlink Implementation using collections deque")
+	public void user_clicks_on_the_hyperlink_implementation_using_collections_deque() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User lands on the Implementation using collections deque page")
+	public void user_lands_on_the_implementation_using_collections_deque_page() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("Queue - User is in Try Editor page from Implementation using collections deque page")
+	public void queue_user_is_in_try_editor_page_from_implementation_using_collections_deque_page() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks on the hyperlink Implementation using array")
+	public void user_clicks_on_the_hyperlink_implementation_using_array() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User lands on the Implementation using array page")
+	public void user_lands_on_the_implementation_using_array_page() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("Queue - User is in Try Editor page from Implementation using array page")
+	public void queue_user_is_in_try_editor_page_from_implementation_using_array_page() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks on the hyperlink Queue Operations")
+	public void user_clicks_on_the_hyperlink_queue_operations() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User lands on the Queue Operations page")
+	public void user_lands_on_the_queue_operations_page() {
+	    throw new io.cucumber.java.PendingException();
+	}
 }
