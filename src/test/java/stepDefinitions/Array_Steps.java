07465fffda585b31fd35e8932e03555d0e9a1d30
shaff <sharmas3658@gmail.com>
2025-05-30 12:40:36 -0400

    added new file

---
+++ src/test/java/stepDefinitions/Array_Steps.java
@@ -0,0 +1,160 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Array_Steps {
+	@Given("The user is in the Home page after Sign in")
+	public void the_user_is_in_the_home_page_after_sign_in() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks the {string} button in Home Page")
+	public void the_user_clicks_the_button_in_home_page(String string) {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user be directed to {string} Data Structure Page")
+	public void the_user_be_directed_to_data_structure_page(String string) {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is in the {string} page after Sign in")
+	public void the_user_is_in_the_page_after_sign_in(String string) {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the practice question editor")
+	public void the_user_is_on_the_practice_question_editor() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the invalid code in Editor and Click the Submit Button")
+	public void the_user_write_the_invalid_code_in_editor_and_click_the_submit_button() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should able to see an NameError message in alert window")
+	public void the_user_should_able_to_see_an_name_error_message_in_alert_window() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the invalid code in the Editor and Click the Run Button")
+	public void the_user_write_the_invalid_code_in_the_editor_and_click_the_run_button() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should able to see error occurred during submission")
+	public void the_user_should_able_to_see_error_occurred_during_submission() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the valid code in the Editor and Click the Run Button")
+	public void the_user_write_the_valid_code_in_the_editor_and_click_the_run_button() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should able to see output in the console")
+	public void the_user_should_able_to_see_output_in_the_console() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the valid code in Editor and Click the Submit Button")
+	public void the_user_write_the_valid_code_in_editor_and_click_the_submit_button() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should able to see error occured during submission")
+	public void the_user_should_able_to_see_error_occured_during_submission() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on the {string} page")
+	public void the_user_is_on_the_page(String string) {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is in the Practice page")
+	public void the_user_is_in_the_practice_page() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to the question\\/{int} page")
+	public void the_user_should_be_redirected_to_the_question_page(Integer int1) {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks \"\"Max Consecutive ones\" link")
+	public void the_user_clicks_max_consecutive_ones_link() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks {string} button in Arrays in Python page")
+	public void the_user_clicks_button_in_arrays_in_python_page(String string) {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to a page having an try Editor with a Run button to test")
+	public void the_user_should_be_redirected_to_a_page_having_an_try_editor_with_a_run_button_to_test() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is in the tryEditor page")
+	public void the_user_is_in_the_try_editor_page() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks the Run Button without entering the code in the Editor")
+	public void the_user_clicks_the_run_button_without_entering_the_code_in_the_editor() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Nothing happens to the page and no error message is displayed")
+	public void nothing_happens_to_the_page_and_no_error_message_is_displayed() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the invalid code in Editor and click the Run Button")
+	public void the_user_write_the_invalid_code_in_editor_and_click_the_run_button() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should able to see an error message in alert window")
+	public void the_user_should_able_to_see_an_error_message_in_alert_window() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the valid code in Editor and click the Run Button")
+	public void the_user_write_the_valid_code_in_editor_and_click_the_run_button() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+}
