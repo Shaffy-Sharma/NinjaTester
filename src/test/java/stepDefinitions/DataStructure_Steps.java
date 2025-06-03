d5aa626419125b842828c2a4b23bd5528c2daacd
shaff <sharmas3658@gmail.com>
2025-06-03 12:26:57 -0400

    added new file

---
+++ src/test/java/stepDefinitions/DataStructure_Steps.java
@@ -0,0 +1,48 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DataStructure_Steps {
+	
+
+
+@When("The user clicks the {string} button in Data Structures - Introduction")
+public void the_user_clicks_the_button_in_data_structures_introduction(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("The user should land in {string}")
+public void the_user_should_land_in(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Given("The user is in the {string} page")
+public void the_user_is_in_the_page(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("The user should be redirected to {string} of Data structures-Introduction")
+public void the_user_should_be_redirected_to_of_data_structures_introduction(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("The user clicks the {string} button")
+public void the_user_clicks_the_button(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("The user should be redirected to a page having an try Editor with a Run button to test")
+public void the_user_should_be_redirected_to_a_page_having_an_try_editor_with_a_run_button_to_test() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+
+}
