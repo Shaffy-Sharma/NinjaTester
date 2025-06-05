455feb8ed0565a2b0ee3f3de4989fc11773b51a9
sunita <patilsunita299@gmail.com>
2025-06-05 17:45:28 -0400

    Committed changes for Graph feature,Graph page,graphSteps,LoggerLoad

--- src/test/java/utilities/LoggerLoad.java
+++ src/test/java/utilities/LoggerLoad.java
@@ -3,7 +3,8 @@ package utilities;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class LoggerLoad {
+public class LoggerLoad
+{
     private static Logger logger = LogManager.getLogger();
 
     public static void info(String message) {
