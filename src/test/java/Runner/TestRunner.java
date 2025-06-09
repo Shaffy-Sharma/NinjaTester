ffb67a669e3f597253964b1cbf16b87bf90fcedb
AyushaGami <patoriyaa@yahoo.in>
2025-06-09 15:20:51 -0400

    Added a tag in feature file

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, 
 		monochrome=false, 
-		tags = " ",
+		tags = "@stack",
 		features = {"src/test/resources/Features"}, 
 		glue= {"stepDefinitions","Hooks"})
 
