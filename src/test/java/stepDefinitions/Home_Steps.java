c56c779efb3e0bd41193d1615931049e88a0646c
akshatakanaje <111020296+akshatakanaje@users.noreply.github.com>
2025-06-16 17:29:04 -0700

    changes in home modules

--- src/test/java/stepDefinitions/Home_Steps.java
+++ src/test/java/stepDefinitions/Home_Steps.java
@@ -1,18 +1,13 @@
 package stepDefinitions;
 
 import Drivers.DriverFactory;
+import PageFactory.GetStarted_Page;
 import PageFactory.Home_Page;
-import PageFactory.Tree_Page;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import utilities.ConfigReader;
 import utilities.ExcelReader;
 import utilities.LoggerLoad;
 import utilities.Utility_Methods;
@@ -25,38 +20,33 @@ public class Home_Steps {
 
     WebDriver driver;
     Home_Page homePage;
-    Utility_Methods util = new Utility_Methods();
+    GetStarted_Page getStartedPage;
 
     public Home_Steps() {
         driver = DriverFactory.getDriver();
         homePage = new Home_Page();
-    }
-
-    @Given("The user is on the DS Algo portal")
-    public void the_user_is_on_the_ds_algo_portal() {
-        LoggerLoad.info("DSAlgo Portal");
-        homePage.dsAlgoPortal();
-    }
-
-    @When("The user clicks the Get Started button")
-    public void the_user_clicks_the_get_started_button() {
-        homePage.clickGetStartedBtn();
-    }
-
-    @Then("The user should be navigated to the Data Structure Introduction page, which displays the Register and Sign in links.")
-    public void the_user_should_be_navigated_to_the_data_structure_introduction_page_which_displays_the_register_and_sign_in_links() {
-        Assert.assertTrue(homePage.isSignInAndRegisterVisible(), "Register and Sign In links are not visible.");
+        getStartedPage = new GetStarted_Page();
     }
 
     @Given("The user is on the Home page")
     public void the_user_is_on_the_home_page() {
-        LoggerLoad.info("User is on the Home Page");
-        homePage.homepage();
+        getStartedPage.dsAlgoPortal();
+        getStartedPage.navigateToHomePage();
+        LoggerLoad.info("Current URL: " + driver.getCurrentUrl());
+        String expectedTitle = "NumpyNinja";
+        String actualTitle = driver.getTitle();
+        LoggerLoad.info("User is on the Home Page. Title: " + actualTitle);
+        Assert.assertEquals(actualTitle, expectedTitle, "User is not on the Home Page.");
     }
+
+//    @When("The user clicks datastructures dropdown and select")
+//    public void the_user_clicks_datastructures_dropdown_and_select() {
+//        homePage.isDropdownVisible();
+//    }
 
     @When("The user clicks datastructures dropdown and select")
     public void the_user_clicks_datastructures_dropdown_and_select() {
-        homePage.isDropdownVisible();
+        LoggerLoad.info("User clicked Data Structures dropdown and selected an option.");
     }
 
     @Then("The error message You are not logged in appears in homepage")
@@ -66,11 +56,11 @@ public class Home_Steps {
 
     @When("The user clicks on Data Structure Get Started button on homepage")
     public void the_user_clicks_on_data_structure_get_started_button_on_homepage() {
-        homePage.clickGetStartedOnHomePage("Data Structures-Introduction");
+        homePage.selectDataStructure();
     }
 
-    @When("The user clicks on signin link")
-    public void the_user_clicks_on_signin_link() {
+    @When("The user clicks on signIn link")
+    public void the_user_clicks_on_signIn_link() {
         LoggerLoad.info("User Clicks on SignIn link");
         homePage.clickSignIn();
     }
@@ -91,7 +81,7 @@ public class Home_Steps {
 
     @Then("The user redirected to Registration page")
     public void the_user_redirected_to_registration_page() {
-        LoggerLoad.info("User redirected to Registraion page ");
+        LoggerLoad.info("User redirected to Registration page ");
         String Title = homePage.registerPage();
         LoggerLoad.info("Title of current page is : " + Title);
         assertEquals(Title, "Registration");
@@ -104,7 +94,7 @@ public class Home_Steps {
 
     @Then("The user should able to see {int} options Arrays, LinkedList, Stack, Queue, Tree, Graph in dropdown menu")
     public void the_user_should_able_to_see_options_arrays_linked_list_stack_queue_tree_graph_in_dropdown_menu(Integer int1) {
-        homePage.validateDataStructuresDropdown(); // Already asserts the count and content
+        homePage.validateDataStructuresDropdown();
     }
 
     @Given("User is in the Home page after logging in using credentials from Excel {int}")
@@ -119,16 +109,21 @@ public class Home_Steps {
 
     @When("The user clicks datastructures dropdown and select {string}")
     public void the_user_clicks_datastructures_dropdown_and_select(String option) {
-        homePage.validateEachDropdownInHomePage(option);
+        LoggerLoad.info("Selecting from dropdown: " + option);
+        homePage.validateDropdownInHomePage(option);
     }
+
 
     @Then("The user should redirected to Array page")
     public void the_user_should_redirected_to_array_page() {
-        homePage.validateNavigationThroughDropdown();
+        String expectedTitle = "Array";
+        String actualTitle = driver.getTitle();
+        LoggerLoad.info("Verifying user is on the Array page. Actual title: " + actualTitle);
+        Assert.assertEquals(actualTitle, expectedTitle, "User is not redirected to the Array page.");
     }
 
     @When("The user clicks on Arrays {string} button on homepage")
     public void the_user_clicks_on_arrays_button_on_homepage(String string) {
-        homePage.clickGetStartedOnHomePage("Arrays");
+        homePage.selectArray();
     }
 }
--- src/test/resources/Features/00_GetStartedPage.feature
+++ src/test/resources/Features/00_GetStartedPage.feature
@@ -1,4 +1,4 @@
-@homePage
+@getStartedPage
 Feature: Homepage
 
   Background: The user is on the dsAlgo portal
@@ -6,4 +6,4 @@ Feature: Homepage
 
   Scenario: Verify that user is able to click on the Get Started button from dsAlgo portal
     When  The user clicks the Get Started button
-    Then  The user should be navigated to the Data Structure Introduction page, which displays the Register and Sign in links.
+    Then The user should land on DSAlgo Homepage.
